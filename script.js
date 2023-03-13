34a967b61da71d0bb5ef181836782eed376fb7d8
Natalina686 <108237928+Natalina686@users.noreply.github.com>
2023-03-13 23:30:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 "use strict";
 
 const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-let initials = userNames.map(split("").substring(0, 1).push("."));
+let initials = userNames.map((name) => {
+return name + ".";
+});
+
 
 // тут ваш код ...
 
 
 console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
-
+// s="text";
+// x=s.split(''); //x=array("t","e","x","t");
 
 // 2 задача
 
@@ -17,10 +21,10 @@ console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 // console.log(arr.map(Number));
 
 // let reverseMaxValue = arr.slice().reverse().join("");
-// // +reverseMaxValue.join(""); чому це не працює?
+//
 
-
-
+// // console.log(+reverseMaxValue.toString());
+// reverseMaxValue = +reverseMaxValue.toString();
 // // // тут ваш код...
 
 // console.log(reverseMaxValue); // 9854
@@ -32,10 +36,8 @@ console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
 
 // const resultsArray = [1, 2, [3, [4]]];
-// let productOfArray = resultsArray([ ...3, ...4]);
-
-
-
-// // тут ваш код...
+// let productOfArray = resultsArray.flatMap(num => num);
+// let result = productOfArray.reduce((acc, rec) => acc * rec);
 
 // console.log(productOfArray); // 24
+// console.log(result);
