3b5945698c32db7916ad48f43c04ed6719b4184d
Nevada686 <natalina686@gmail.com>
2023-03-21 14:55:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 "use strict";
 
-const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-let initials = userNames.map(name => {
+const userNames = [
+  "Петрик Ольга Іванівна",
+  "Гнатюк Петро Антонович",
+  "Рудко Андрій Опанасович",
+];
+
+let initials = userNames.map((name) => {
   // далі можно за допомогою split це розділити
   const words = name.split(" "); // отримаємо масив слів
   // ...
-for (const word of words)
-   console.log(word[0]);
+  console.log(words);
 
+  const wordsFirstLetters = words.map((word) => word[0]);
+  console.log(wordsFirstLetters);
+  const letters = wordsFirstLetters.join(".");
+  console.log(letters);
+  console.log(typeof letters);
+  let lettersArray = letters.split("-");
+  console.log(lettersArray);
+});
 
-}
-);
+// console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
-
-
-
-
-
-console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
-
-
-
-
-
+// console.log(lettersArray);
 
 // 2 задача
-
 
 // const currentMaxValue = 4589;
 // let arr = ("" + currentMaxValue).split("");
@@ -41,10 +41,7 @@ console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 // console.log(reverseMaxValue); // 9854
 // console.log(typeof reverseMaxValue); // 'number'
 
-
-
 // 3 задача
-
 
 // const resultsArray = [1, 2, [3, [4]]];
 // let productOfArray = resultsArray.flatMap(num => num);
