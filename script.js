5db43d3cdea7e802c3ce002263218dafad68f92c
Nevada686 <natalina686@gmail.com>
2023-03-24 09:11:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 "use strict";
 
-const userNames = [
-  "Петрик Ольга Іванівна",
-  "Гнатюк Петро Антонович",
-  "Рудко Андрій Опанасович",
-];
+// const userNames = [
+//   "Петрик Ольга Іванівна",
+//   "Гнатюк Петро Антонович",
+//   "Рудко Андрій Опанасович",
+// ];
 
-let initials = userNames.map((name) => {
-  const words = name.split(" ");
+// let initials = userNames.map((name) => {
+//   const words = name.split(" ");
 
-  // console.log(words);
+//   // console.log(words);
 
-  const wordsFirstLetters = words.map((word) => word[0]);
+//   const wordsFirstLetters = words.map((word) => word[0]);
 
-  // console.log(wordsFirstLetters);
-  const letters = wordsFirstLetters.join(".");
-  return letters;
-});
+//   // console.log(wordsFirstLetters);
+//   const letters = wordsFirstLetters.join(".");
+//   return letters;
+// });
 
-console.log(initials.sort()); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
+// console.log(initials.sort()); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
 // 2 задача
 
@@ -43,11 +43,51 @@ console.log(initials.sort()); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 // продвинута робота з функціями
 
 // перша задача
+// function getDifferences(start, end) {
+//   const DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
 
-// let firstDate = Date.now(2005, 05, 25, 20, 03, 33);
-// // doSomethingForALongTime();
-// let secondDate = Date.now(2025, 08, 30, 20, 58, 22);
+//   const firstDate = Date.parse(start);
+//   const secondDate = Date.parse(end);
 
-// let totalTime = secondDate - firstDate;
+//   console.log(firstDate);
+//   console.log(secondDate);
+//   const difference = secondDate - firstDate;
+//   console.log("result", difference);
+//   if (firstDate > secondDate) {
+//     return Math.abs(difference);
+//   }
+//   return difference / DAY_IN_MILLISECONDS;
+// }
 
-// console.log(totalTime);
+// console.log(getDifferences("2003-03-10", "2023-03-23")); // 3
+// console.log(getDifferences("2002-03-01", "2023-03-13")); // 12
+// console.log(getDifferences("2026-03-17", "2023-03-13")); // 12
+
+// second task
+
+const priceData = {
+  Apples: "23.4",
+  BANANAS: "48",
+  oRAngGEs: "48.7584",
+};
+// Чому не працює?
+// const result = Object.entries(priceData).map(([key, value]) => [
+//   (key = key.toLowerCase),
+//   (value = value.toPrecision(2)),
+// ]);
+// const resultObject = Object.fromEntries(result);
+// console.log(resultObject);
+
+function optimizer(data) {
+  const keys = Object.keys(data);
+  for (const key of keys) {
+    console.log(keys);
+    if (typeof key === "string") {
+      return key.toLowerCase();
+    }
+    return keys;
+  }
+}
+
+let updatedPriceData = optimizer(priceData);
+console.log(updatedPriceData); // {apples: '23.40', bananas: '48.00', oranges: '48.76'}
