d06467abff7ace472d6dc9972526b81c6dc74d66
Natalina686 <108237928+Natalina686@users.noreply.github.com>
2023-04-17 15:57:38 +0200

    3 task

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 "use strict";
 
-const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-let initials = userNames.map(name => {
-  // далі можно за допомогою split це розділити
-  const words = name.split(" "); // отримаємо масив слів
-  // ...
-for (const word of words)
-   console.log(word[0]);
+// const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
+// let initials = userNames.map(name => {
+//   // далі можно за допомогою split це розділити
+//   const words = name.split(" "); // отримаємо масив слів
+//   // ...
+// for (const word of words)
+//    console.log(word[0]);
 
 
-}
-);
+// }
+// );
 
 //   const wordsFirstLetters = words.map((word) => word[0]);
 
@@ -22,7 +22,7 @@ for (const word of words)
 
 
 
-console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
+// console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
 
 
@@ -52,51 +52,97 @@ console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 // продвинута робота з функціями
 
 // перша задача
-// function getDifferences(start, end) {
-//   const DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
 
-//   const firstDate = Date.parse(start);
-//   const secondDate = Date.parse(end);
+function getDifferences(
+  start = new Date(),
+  end = new Date(),
+  period = "hours"
+) {
+  const DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
+  const HOUR_IN_MILLISECONDS = 60 * 60 * 1000;
+  const MIN_IN_MILLISECONDS = 60 * 1000;
+  const SEC_IN_MILLISECONDS = 1000;
+  const startDate = Date.parse(start);
+  const endDate = Date.parse(end);
+  let resultMillisec = Math.abs(startDate - endDate);
+  let result;
+  switch (period) {
+    case "seconds":
+      result = resultMillisec / SEC_IN_MILLISECONDS;
+      break;
+    case "minuts":
+      result = resultMillisec / MIN_IN_MILLISECONDS;
+      break;
+    case "hours":
+      result = resultMillisec / HOUR_IN_MILLISECONDS;
+      break;
+    case "days":
+      result = resultMillisec / DAY_IN_MILLISECONDS;
+      break;
+  }
+  console.log(`${result} ${period}`);
+  return `${result} ${period}`;
+}
+getDifferences("2002-03-01", "2023-03-13", "seconds");
+getDifferences("2003-03-10", "2023-03-23", "hours");
+getDifferences("2026-03-17", "2023-03-13", "days");
 
-//   console.log(firstDate);
-//   console.log(secondDate);
-//   const difference = secondDate - firstDate;
-//   console.log("result", difference);
-//   if (firstDate > secondDate) {
-//     return Math.abs(difference);
-//   }
-//   return difference / DAY_IN_MILLISECONDS;
-// }
 
-// console.log(getDifferences("2003-03-10", "2023-03-23")); // 3
-// console.log(getDifferences("2002-03-01", "2023-03-13")); // 12
-// console.log(getDifferences("2026-03-17", "2023-03-13")); // 12
 
 // second task
 
-const priceData = {
-  Apples: "23.4",
-  BANANAS: "48",
-  oRAngGEs: "48.7584",
-};
-// Чому не працює?
+// const priceData = {
+//   Apples: "23.4",
+//   BANANAS: "48",
+//   oRAngGEs: "48.7584",
+// };
+// // Чому не працює? Якщо без toFixed працює, як це правильно написати?
+
 // const result = Object.entries(priceData).map(([key, value]) => [
-//   (key = key.toLowerCase),
-//   (value = value.toPrecision(2)),
+//   (key = key.toLowerCase()),
+  
+//  (value = +value.toString().toFixed(2)),
+ 
 // ]);
+// console.log(result);
+
 // const resultObject = Object.fromEntries(result);
+
 // console.log(resultObject);
 
-function optimizer(data) {
-  const keys = Object.keys(data);
-  for (const key of keys) {
-    console.log(keys);
-    if (typeof key === "string") {
-      return key.toLowerCase();
-    }
-    return keys;
-  }
-}
+// console.log(updatedPriceData); // {apples: '23.40', bananas: '48.00', oranges: '48.76'}
 
-let updatedPriceData = optimizer(priceData);
-console.log(updatedPriceData); // {apples: '23.40', bananas: '48.00', oranges: '48.76'}
+
+
+
+// Задача про рекурсію та ітераціюНапишіть:
+// - функцію яка рекурсивно буде знаходити суму всіх непарних додатніх чисел до якогось числа.
+// Приклад:
+// function recursiveOddSumTo(number) { 
+// // let total = 0;
+// // if (number % 2 !== 0) return total += number;
+  
+// }
+// console.log(recursiveOddSumTo(1)) // 1
+// console.log(recursiveOddSumTo(10)) // 25
+
+
+
+// - функцію яка ітеративно (в циклі) буде знаходити суму всіх непарних додатніх чисел до якогось числа.
+// Приклад:
+
+// тут ваш код};
+
+
+
+function iterativeOddSumTo(number) {
+let result = 0;
+for (let i = 0; i < number; i++) {
+  result += number;
+}
+  return result;
+  
+  };
+
+// сonsole.log(iterativeOddSumTo(1)) // 1
+console.log(iterativeOddSumTo(10)) // 25
