1f12de80349eacd233acf2310e7b72a6ea0cd295
Natalina686 <108237928+Natalina686@users.noreply.github.com>
2023-04-26 21:15:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -125,31 +125,31 @@
 // Задача про рекурсію та ітераціюНапишіть:
 // - функцію яка рекурсивно буде знаходити суму всіх непарних додатніх чисел до якогось числа.
 // Приклад:
-// function recursiveOddSumTo(number) { 
-//   if (number === 1){
-//   return 1;
-// }
-//  if (number % 2 === 0) {
-//   return recursiveOddSumTo(number-1);
-//  }
-//  if (number % 2 !== 0) {
-//   return number + recursiveOddSumTo(number-2);
-// }
-
-// }
-// console.log(recursiveOddSumTo(1)) // 1
-// console.log(recursiveOddSumTo(10)) // 25
-
-function iterativeOddSumTo(number) {
-  if (number <= 0) return 0;
-  let sum = 0;
-
-  for (let i = 1; i <= number; i += 2) {
-  sum += i;
+function recursiveOddSumTo(number) { 
+  if (number === 1){
+  return 1;
+}
+ if (number % 2 === 0) {
+  return recursiveOddSumTo(number-1);
+ }
+ else {
+  return number + recursiveOddSumTo(number-2);
 }
 
-  return sum;
 }
+console.log(recursiveOddSumTo(1)) // 1
+console.log(recursiveOddSumTo(10)) // 25
 
- console.log(iterativeOddSumTo(1)) // 1
- console.log(iterativeOddSumTo(10)) // 25
+// function iterativeOddSumTo(number) {
+//   if (number <= 0) return 0;
+//   let sum = 0;
+
+//   for (let i = 1; i <= number; i += 2) {
+//   sum += i;
+// }
+
+//   return sum;
+// }
+
+//  console.log(iterativeOddSumTo(1)) // 1
+//  console.log(iterativeOddSumTo(10)) // 25
